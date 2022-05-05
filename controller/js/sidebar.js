ffcf1ca3817ca3cb797eb23ac9473b6124dfa7e5
andrew <andrew@cloud-latitude.com>
2022-05-05 16:20:23 +0800

    sidebar

--- controller/js/sidebar.js
+++ controller/js/sidebar.js
@@ -1,24 +1,25 @@
-$(document).ready(function() {
+$(document).ready(function () {
     /*-li active-*/
-    let activecate = 'cate' + sessionStorage.getItem('sidebar');
+    let activecate = 'cate' + sessionStorage.getItem('sidebar')
 
-    document.getElementById(activecate).classList.add('active');
+    document.getElementById(activecate).classList.add('active')
+    document.querySelector("#" + activecate + ' .icon-wrapper img').src = './assets/images/icons/' + activecate + '_active.png'
 
 
-    var obj = get_user_id();
+    var obj = get_user_id()
 
     /*-show name & id-*/
-    $('.name-id').html(obj);
+    $('.name-id').html(obj)
 
     if ($(window).width() < 1200)
-        $('#sidebar').removeClass('active');
-});
+        $('#sidebar').removeClass('active')
+})
 
 function check_user() {
 
 }
 
 function get_user_id() {
-    var log = ajax(0, '0.0.1');
-    return log[0];
+    var log = ajax(0, '0.0.1')
+    return log[0]
 }
