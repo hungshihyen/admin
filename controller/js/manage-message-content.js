1e2fae7057d360f976bd796aafb46fa7ffb0dd7d
andrew <andrew@cloud-latitude.com>
2022-05-05 17:39:44 +0800

    message

--- controller/js/manage-message-content.js
+++ controller/js/manage-message-content.js
@@ -13,9 +13,9 @@ function set_table() {
     console.log(arr);
 
     temp = $($('template').html()).clone();
-    $('#time').html('<p style="color: #AEAEAE;">' + arr[0]['pw_time'] + '</p>');
-    $('#name').html('<p style="color: #AEAEAE;">' + arr[0]['m_name']) + '</p>';
-    $('#mtxt').html('<p tyle="color: #AEAEAE;">' + arr[0]['pw_destxt'] + '</p>');
+    $('#time').html('<p style="color: #747474;">' + arr[0]['pw_time'] + '</p>');
+    $('#name').html('<p style="color: #747474;">' + arr[0]['m_name']) + '</p>';
+    $('#mtxt').html('<p style="color: #747474;">' + arr[0]['pw_destxt'] + '</p>');
     $('#img').html(`<img src="${arr[0]['pw_picpath']}"  alt="Face 1" class="w-75">`);
 
     $('div #ta').append($temp);
